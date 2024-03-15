0378886f97efaf715af4ffa5093f55565b6f6255
Nazarii Stakhovskyi <Nazar Stakhovskyi>
2024-03-15 22:17:08 +0200

    refactor birthday lookup function

--- personal_assistant/src/utils.py
+++ personal_assistant/src/utils.py
@@ -15,7 +15,7 @@ def wrong_input_handling(function):
         try:
             return function(*args, **kwargs)
         except WrongInfoException as err:
-            return err
+            return err.args[0]
         except WrongDate as err:
             return err.args[0]
         except WrongEmail as err:
@@ -42,7 +42,7 @@ def wrong_input_handling(function):
 
 def get_contact(book, name):
     try:
-        contact = book.data[name]
+        contact = book.contacts[name]
         return contact
     except KeyError:
         raise KeyError(f"The contacts book doesn't have a contact named {name}")
@@ -119,14 +119,14 @@ def parser(user_input):
     return command, *args, None
 
 
-def get_birthdays_per_week(users: list[any]):
-    bds_seven_days = collections.defaultdict(list)
+def get_birthdays_num_days(users: list[any], days: int = ...):
+    bds_num_days = collections.defaultdict(list)
     current_date = datetime.today().date()
     birthdays = []
 
     for user in users:
         if user.birthday:
-            user_bd = user.birthday.birthday.date()
+            user_bd = user.birthday.value.date()
             bd_this_year = user_bd.replace(year=current_date.year)
         else:
             continue
@@ -136,62 +136,26 @@ def get_birthdays_per_week(users: list[any]):
 
         days_delta = (bd_this_year - current_date).days
 
-        if days_delta < 7:
+        if days_delta < days:
             day_to_congrats = bd_this_year.strftime("%A")
 
             ## if the function is run on Sunday and the BD is in 6 days
-            if days_delta == 6 and day_to_congrats in ["Saturday", "Sunday"]:
+            if days_delta == days - 1 and day_to_congrats in ["Saturday", "Sunday"]:
                 day_to_congrats = "Next Monday"
             elif day_to_congrats in ["Saturday", "Sunday"]:
                 day_to_congrats = "Monday"
 
-            bds_seven_days[day_to_congrats].append(user.name.name)
-
-    if len(bds_seven_days) > 0:
-        ## print out the list of names per day for the next seven days
-        for day, names in bds_seven_days.items():
-            birthdays.append("{:<15}{:<5}{}".format(day, ":", ", ".join(names)))
+            bds_num_days[day_to_congrats].append([user.name.value, user.birthday.value])
+    if len(bds_num_days) > 0:
+        ## print out the list of names per day for the next num days
+        for day, names in bds_num_days.items():
+            for name in names:
+                date_of_birth = name[1].strftime("%Y-%m-%d")
+                birthdays.append(
+                    "{:<10} | {:^15} : {:^15}".format(day, date_of_birth, name[0])
+                )
     else:
-        birthdays.append("No birthdays next week.")
+        birthdays.append(f"No birthdays within the next {days} days.")
 
     ## in case the list is needed elsewhere
     return birthdays
-
-
-## test the function
-if __name__ == "__main__":
-    test_date = datetime.today()
-
-    users = [
-        {
-            "name": "Bill Gates",
-            "birthday": test_date.replace(
-                month=test_date.month - 1
-            ),  ## BD this year last month
-        },
-        {
-            "name": "Bill States",
-            "birthday": test_date.replace(month=12),  ## BD this year end of year
-        },
-        {
-            "name": "Bill Mates",
-            "birthday": test_date.replace(day=test_date.day - 1),  ## BD yesterday
-        },
-        {
-            "name": "Bill Rates",
-            "birthday": test_date.replace(day=test_date.day + 1),  ## BD tomorrow
-        },
-        {"name": "Bill Wates", "birthday": test_date},  ## BD today
-        {
-            "name": "Bill Spades",
-            "birthday": test_date.replace(
-                day=test_date.day + 6
-            ),  ## BD weekend; needs adjusting
-        },
-        {
-            "name": "Bill Dates",
-            "birthday": test_date.replace(day=test_date.day + 7),  ## BD in a week
-        },
-    ]
-    print(users[0])
-    get_birthdays_per_week(users)
